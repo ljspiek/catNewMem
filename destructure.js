b6717fc3258c40f5b0773558941f0a75c988c0b3
Laura Spieker <ljspiek@gmail.com>
2020-06-28 16:39:09 -0500

    solved with destructuring and .map

---
+++ destructure.js
@@ -0,0 +1,14 @@
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
+
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ])
+);
